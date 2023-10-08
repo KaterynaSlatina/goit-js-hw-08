2d0750738f26af3545a2e0f07577876a607dc551
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-10-08 15:15:36 +0300

    video, feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from "lodash.throttle";
 
+const FEEDBACK_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+
+const formData = {
+email: "email.value",
+message: "message.value",
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem('FEEDBACK_KEY', JSON.stringify(formData));
+}
+
+function inTextarea() {
+    const savedMassage = JSON.parse(localStorage.getItem('FEEDBACK_KEY'));
+    if (savedMassage === null) {
+        return
+    }
+}
